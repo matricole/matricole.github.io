8e393d20ac8c0b34c2618cc92be7803fd33595b0
matricole <matricolenetwork@gmail.com>
2020-09-15 14:59:50 +0200

    Update search.js and search.json

--- bicocca/js/search.js
+++ bicocca/js/search.js
@@ -1,8 +1,45 @@
 var source = null;
 var result = null;
+var json_global = null;
+
+function loadJson(){
+	json_global = [];
+	json_global.push(
+		{id: "7", name: "Tizio Caio", type: "tg"},
+		{id: "8", name: "Mario Caio", type: "tg"},
+	);
+	console.log(json_global);
+}
 
 const inputHandler = function(e) {
 inputHandlerMethod(e);
+}
+
+function calculateResults(inputtext_words, searchInto){
+	if (inputtext_words == null || searchInto == null)
+		return null;
+	
+	if (searchInto.length == 0)
+		return null;
+	
+	if (inputtext_words.length == 0)
+		return searchInto;
+	
+	var results = [];
+	
+	var input_single = inputtext_words[0].toLowerCase();
+	var i;
+	for (i = 0; i < searchInto.length; i++) {
+	  var valueToSearch = searchInto[i];
+	  var name = valueToSearch["name"].toLowerCase();
+	  if (name.includes(input_single))
+	  {
+		  results.push(valueToSearch);
+	  }
+	} 
+		
+	inputtext_words.shift();
+	return calculateResults(inputtext_words, results);
 }
 
 function inputHandlerMethod(e) {
@@ -18,16 +55,35 @@ function inputHandlerMethod(e) {
     var inputtext = e.target.value;
   var inputtext_words = inputtext.trim().split(" ");
 
-
-	result.innerHTML = inputtext_words.length;  
+	var results_calculated = calculateResults(inputtext_words, json_global);
+	if (results_calculated == null || results_calculated.length == 0)
+	{	
+		result.innerHTML = "Nessun risultato! " + inputtext_words.length;  
+		return;
+	}
+	
+	var i;
+	var stringResult = "<ul style='margin-bottom:0px;'>"
+	for (i = 0; i < results_calculated.length; i++) {
+			stringResult += "<li>";
+		stringResult += results_calculated[i]["name"];
+			stringResult += "</li>";
+	}
+	stringResult += "</ul>";
+	result.innerHTML = stringResult;  
 }
 
 $(document).ready(function() {
+
 
 	result = document.getElementById('result_search');
 	source = document.getElementById('input_search');
 	source.addEventListener('input', inputHandler);
 	source.addEventListener('propertychange', inputHandler); // for IE8
 	inputHandlerMethod(null);
+	loadJson();
+
 });
+
+
 
